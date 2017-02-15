e744ca7d0016774350b75c642457269dcfafced3
kuangfen <kuangfen@fulan.com.cn>
2017-02-15 15:59:28 +0800

    stash_更新提交

--- git_demo/src/com/foodoo/test/Demo.java
+++ git_demo/src/com/foodoo/test/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("demo_braches_1");
+		System.out.println("add stash_demo");
+		
 	}
 
 }
