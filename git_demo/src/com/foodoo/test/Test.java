b284a759b70a8c22547d92fe479a88b8801f95be
kuangfen <kuangfen@fulan.com.cn>
2017-02-17 16:58:15 +0800

    master

--- git_demo/src/com/foodoo/test/Test.java
+++ git_demo/src/com/foodoo/test/Test.java
@@ -3,8 +3,6 @@ package com.foodoo.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello git!!!");
-		System.out.println("update");
 		System.out.println("master");
 		
 	}
